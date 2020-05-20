eb636751c2edd5f77b2bbc842decb16bb10097ca
AavulaManudeep <manudeep024@gmail.com>
2020-05-19 21:28:23 -0400

    Bootstrap features and Dashboard intial step

--- cableproject/src/Components/Home.js
+++ cableproject/src/Components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Doughnut} from 'react-chartjs-2';
+import {Container, Row, Col,} from 'react-bootstrap';
+
 function Home() {
 
   const state = {
-    labels:["UnPaid","Paid"],
+    labels:["To be collected ","Collected"],
     datasets :[{
       label: 'Summery',
       borderWidth:6,
@@ -19,15 +21,34 @@ function Home() {
       data: [50,30]
     }]
   }
+  const state2 = {
+    labels:["UnPaid Customers","Paid Customers"],
+    datasets :[{
+      label: 'Summery',
+      borderWidth:6,
+      backgroundColor: [
+        '#B21F00',
+        '#2FDE00'
+      ],
+      borderColor:'black',
+      hoverBackgroundColor: [
+      '#501800',
+      '#175000',
+      ],
+      data: [80,20]
+    }]
+  }
     return (
         <div>
-          <div className="">
-          <Doughnut
+          <Container>
+            <Row>
+              <Col>
+              <Doughnut
           data={state}
           options={{
             title:{
+              text:"Total collection vs To be collected",
               display:true,
-              text:"Average Rainfall",
               fontSize:20,
             },
             cutoutPercentage:0,
@@ -39,12 +60,40 @@ function Home() {
             },
             legend:
             {
-              display:false,
+              display:true,
+              position:"right",
+              fontSize:10,
+            }
+          }}
+          />
+              </Col>
+              <Col>
+              <Doughnut
+          data={state2}
+          options={{
+            title:{
+              text:"Total customers paid vs unpaid",
+              display:true,
+              fontSize:20,
+            },
+            cutoutPercentage:0,
+            circumference:2*Math.PI,
+            rotation:-0.5*Math.PI,
+            animation:{
+                animateRotate:true,
+                animateScale:true   
+            },
+            legend:
+            {
+              display:true,
               position:"right"
             }
           }}
           />
-          </div>
+              </Col>
+            </Row>
+          </Container>
+          
         </div>
     )
 }
